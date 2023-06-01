2bbe89489f37e0e867c9da462fa9099903c87208
User <email@someemail.com>
2023-06-01 17:21:46 +0600

    fix

--- src/main/java/Forth.java
+++ src/main/java/Forth.java
@@ -2,6 +2,6 @@ import java.util.Date;
 
 public class Forth {
     void printCurrentTime() {
-        System.out.println("Current time: " + new Date())
+        System.out.println("Current time: " + new Date());
     }
 }
